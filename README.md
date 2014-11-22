ce29a7bf0f5f8511d3376a11c50599fa120e811c
bubkoo <bubkoo@163.com>
2014-11-22 21:45:28 +0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # grunt-underscore-compiler
 
-> Complie underscore templates into JavaScript.
+> Complie underscore templates into JavaScript files.
 
 ## Getting Started
 This plugin requires Grunt `~0.4.5`
@@ -17,14 +17,14 @@ Once the plugin has been installed, it may be enabled inside your Gruntfile with
 grunt.loadNpmTasks('grunt-underscore-compiler');
 ```
 
-## The "underscore_compiler" task
+## The "underscore" task
 
 ### Overview
 In your project's Gruntfile, add a section named `underscore_compiler` to the data object passed into `grunt.initConfig()`.
 
 ```js
 grunt.initConfig({
-  underscore_compiler: {
+  underscore: {
     options: {
       // Task-specific options go here.
     },
@@ -39,44 +39,118 @@ grunt.initConfig({
 
 #### options.separator
 Type: `String`
-Default value: `',  '`
+Default value: `linefeed + linefeed`
 
-A string value that is used to do something with whatever.
+Concatenated files will be joined on this string.
 
-#### options.punctuation
+#### options.indent
 Type: `String`
-Default value: `'.'`
+Default value: `    `
 
-A string value that is used to do something else with whatever else.
+By default 4 space indent is used for the generated code. 
+
+#### options.quoteChar
+Type: `String`
+Default value: `'`
+
+Strings are quoted with single quote-only by default. However, for projects that want strict double-quotes usage, you can specify:
+
+```js
+options: {
+    quoteChar: '"'
+}
+```
+
+#### options.raw
+Type: `Boolean`
+Default value: `true`
+
+By default keep the line break for the generated code. You can specify this option to `false` to get one line code, but you also shoule note that this option is not equal to htmlmin. 
+
+#### options.namespace
+Type: `String` or `false` or `Function`
+Default value: `app.tpl`
+
+The namespace in which the precompiled templates will be assigned. Use dot notation (e.g. App.Templates) for nested namespaces or false for no namespace wrapping. When false with amd option set true, templates will be returned directly from the AMD wrapper.
+
+Example:
+
+```js
+options: {
+  namespace: 'MyApp.Templates'
+}
+```
+You can generate nested namespaces based on the file system paths of your templates by providing a function. The function will be called with one argument (the template filepath). The function must return a dot notation based on the filepath.
+
+Example:
+
+```js
+options: {
+  namespace: function(filename) {
+    var names = filename.replace(/modules\/(.*)(\/\w+\.hbs)/, '$1');
+    return names.split('/').join('.');
+  },
+  files: {
+    'ns_nested_tmpls.js' : [ 'modules/**/*.hbs']
+  }
+}
+```
+
+#### options.global
+Type: `String`
+Default value: `this`
+
+The global object which the namespace belong to. 
+
+Example:
+
+```js
+options: {
+  namespace: 'MyApp.Templates',
+  global: 'this'
+}
+```
+
+The generated code will like:
+
+
+```js
+this[MyApp][Templates][SomeName] = 'your template';
+```
+
+#### options.amd
+Type: `String` or `Boolean` or `Array`
+Default value: `false`
+
+Wraps the output file with an AMD define function and returns the compiled template namespace unless namespace has been explicitly set to false in which case the template function will be returned directly.
+
+If String then that string will be used in the module definition `define(['your_amd_opt_here'])`.
+
+If Array then those strings will be used in the module definition. 
+
+#### options.compile
+Type: `Boolean`
+Default value: `false`
+
+By default return the HTML string. Set to `true` will get precompiled template function.
+
+#### options.templateSettings
+Type: `Object`
+Default value: `{}`
+
+[underscore](http://underscorejs.org/#template)'s templateSettings.
 
 ### Usage Examples
 
-#### Default Options
-In this example, the default options are used to do something with whatever. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result would be `Testing, 1 2 3.`
-
 ```js
 grunt.initConfig({
-  underscore_compiler: {
-    options: {},
-    files: {
-      'dest/default_options': ['src/testing', 'src/123'],
-    },
-  },
-});
-```
-
-#### Custom Options
-In this example, custom options are used to do something else with whatever else. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result in this case would be `Testing: 1 2 3 !!!`
-
-```js
-grunt.initConfig({
-  underscore_compiler: {
+  underscore: {
     options: {
-      separator: ': ',
-      punctuation: ' !!!',
+        namespace: 'MyApp.Templates'
     },
     files: {
-      'dest/default_options': ['src/testing', 'src/123'],
+      'path/to/result.js': 'path/to/source.html',
+      'path/to/another.js': ["path/to/sources/*.html', 'path/to/more/*.html']
     },
   },
 });
@@ -86,4 +160,11 @@ grunt.initConfig({
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
 
 ## Release History
-_(Nothing yet)_
+
+- 2014-11-22   v0.0.1   Complie underscore templates into JavaScript files.  
+
+---
+
+Task submitted by [bubkoo](http://bubkoo.com)
+
+*This file was generated on Sun Nov 09 2014 13:49:22.*
---
+++ tasks/underscore.js
@@ -0,0 +1,248 @@
+/*
+ * grunt-underscore-compiler
+ * 
+ *
+ * Copyright (c) 2014 bubkoo
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var _ = require('underscore');
+
+module.exports = function (grunt) {
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
+
+    grunt.registerMultiTask('underscore', 'Compile underscore templates into JavaScript files.', function () {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            separator: grunt.util.linefeed + grunt.util.linefeed,
+            indent   : '    ',
+            quoteChar: '\'',
+            raw      : true,
+            namespace: 'app.tpl',
+            global   : 'this',
+            amd      : false,
+            compile  : false,
+
+            templateSettings: {}
+        });
+
+        var filesCount = 0;
+        var quoteChar = options.quoteChar;
+        var processName = options.processName || defaultProcessName;
+        var hasNamespace = options.namespace !== false;
+        var isNamespaceFn = _.isFunction(options.namespace);
+        var nsObject;
+
+        if (hasNamespace && !isNamespaceFn) {
+            nsObject = nsDeclare(options.namespace, options.global, options.quoteChar);
+        }
+
+        // Iterate over all specified file groups.
+        this.files.forEach(function (f) {
+            var nsDefined = [];
+            var nsConflict = [];
+            var isFirstDefine = true;
+
+            var result = f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function (filepath) {
+                // Read file source.
+                var source = grunt.file.read(filepath);
+                var result;
+                if (options.compile === true) {
+                    result = _.template(source, options.templateSettings).source;
+                    result = result.replace(/\r?\n/g, '');
+                    result += ';';
+                } else {
+                    result = html2string(source, quoteChar, options.raw,
+                            options.amd || hasNamespace ? options.indent + options.indent : '');
+                    result = quoteChar + result + quoteChar + ';';
+                }
+
+
+                // Handle namespace
+                var namespace;
+                var nsDefine = '';
+                if (hasNamespace) {
+                    if (isNamespaceFn) {
+                        nsObject = nsDeclare(
+                            options.namespace(filepath),
+                            options.global,
+                            quoteChar);
+                    }
+
+                    if (isNamespaceFn || isFirstDefine) {
+                        nsObject.defines.forEach(function (df) {
+                            if (nsDefined.indexOf(df) === -1) {
+                                nsDefined.push(df);
+
+                                // Set indent
+                                if (options.amd) {
+                                    nsDefine += options.indent;
+                                }
+
+                                nsDefine += df + grunt.util.linefeed;
+                            }
+                        });
+                        nsDefine += grunt.util.linefeed;
+                        isFirstDefine = false
+                    }
+
+                    if (nsConflict.indexOf(nsObject.namespace) === -1) {
+                        nsConflict.push(nsObject.namespace);
+                    }
+
+                    namespace = nsObject.namespace +
+                        '[' + quoteChar + processName(filepath) + quoteChar + '] = ';
+
+                    // Set indent
+                    if (options.amd) {
+                        namespace = options.indent + namespace;
+                    }
+
+                    result = nsDefine + namespace + result;
+                }
+
+                if (options.amd && !hasNamespace) {
+                    result = options.indent + 'return ' + result;
+                }
+
+                return result;
+
+            }).join(grunt.util.normalizelf(options.separator));
+
+            var modules;
+            var amdString;
+
+            if (options.amd) {
+                amdString = 'define(';
+                if (typeof options.amd === 'boolean') {
+                    modules = [];
+                } else if (typeof options.amd === 'string') {
+                    modules = [options.amd];
+                } else if (Array.isArray(options.amd)) {
+                    modules = options.amd;
+                }
+                var length = modules.length;
+                if (length) {
+                    amdString += '[';
+                    amdString += quoteChar + modules.join(quoteChar + ', ' + quoteChar) + quoteChar;
+                    amdString += '], ';
+                }
+                amdString += 'function (';
+                amdString += modules.join(', ');
+                amdString += ') {' + grunt.util.linefeed;
+
+                result = amdString + result;
+                result += grunt.util.linefeed;
+                result += grunt.util.linefeed;
+                result += options.indent + 'return ' + getTopNamespace(nsConflict) + ';';
+                result += grunt.util.linefeed;
+                result += '});';
+            }
+
+            filesCount++;
+            // Write the destination file.
+            grunt.file.write(f.dest, result);
+            grunt.verbose.writeln('File ' + f.dest + ' created.');
+        });
+        // Print a success message.
+        grunt.log.ok(filesCount + ' ' + grunt.util.pluralize(filesCount, 'file/files') + ' created.');
+    });
+
+    // Helpers
+    // -------
+    function html2string(html, quoteChar, raw, indent) {
+        var line;
+        var rBase = new RegExp('\\\\', 'g');
+        var rQuote = new RegExp('\\' + quoteChar, 'g');
+
+        if (raw) {
+            // Keep line break
+            line = '\\n' + quoteChar + ' +' + grunt.util.linefeed + indent + quoteChar;
+        } else {
+            line = '\\n';
+        }
+
+        return html
+            .replace(rBase, '\\\\')
+            .replace(rQuote, '\\' + quoteChar)
+            .replace(/\r?\n/g, line);
+    }
+
+    function nsDeclare(ns, root, quoteChar) {
+        var result = {
+            namespace: root,
+            defines  : []
+        };
+        if (ns) {
+            var parts = ns.split('.');
+            var namespace = root;
+            if (parts[0] === root) {
+                parts.shift();
+            }
+
+            var defines = parts.map(function (part) {
+                namespace = namespace + '[' + quoteChar + part + quoteChar + ']';
+                return namespace + ' = ' + namespace + ' || {};'
+            });
+
+            result.namespace = namespace;
+            result.defines = defines;
+        }
+        return result;
+    }
+
+    function getTopNamespace(namespaces) {
+        // this[app][tpl] => this[app][tpl]
+        // this[app][tpl1], this[app][tpl2] => this[app]
+        // this[app1][tpl], this[app2][tpl] => this
+        var length = namespaces.length;
+        if (length === 1) {
+            return namespaces[0];
+        }
+        var parts = [];
+        var sections = namespaces.map(function (ns) {
+            return ns.split('[');
+        });
+
+        var part;
+        var matched;
+        for (var i = 0, l = sections[0].length; i < l; i++) {
+
+            part = sections[0][i];
+
+            for (var j = 1, k = length; j < k; j++) {
+                if (i > sections[j].length - 1) {
+                    matched = false;
+                    break;
+                }
+                matched = part === sections[j][i];
+                if (!matched) {
+                    break;
+                }
+            }
+            if (matched) {
+                parts.push(part);
+            } else {
+                break;
+            }
+        }
+
+        return parts.join('[');
+    }
+
+    // filename conversion for templates
+    function defaultProcessName(name) {
+        return name;
+    }
+};
